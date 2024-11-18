bbaaadb4302b51965f38a2d254c0bb2d9672e5af
omar538 <125133978+omar538@users.noreply.github.com>
2024-11-18 19:52:20 +0000

    container fig done

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,14 @@
+const accountId = 4445
+let accountName = "moonservices@gmail.com"
+var accountPassword ="1234"
+  AccountCity = "Islamabad"
+
+//   accountId = 2
+
+  console.log(accountId);
+  
+  accountName = "noservice@gmail.com"
+ accountPassword ="5566"
+    AccountCity = "Lahore"
+
+    console.table([accountName,accountPassword,AccountCity])
