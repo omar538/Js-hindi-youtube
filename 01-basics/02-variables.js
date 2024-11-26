8aaf79843fd778cba624c170164c123aed0bbd98
omar538 <125133978+omar538@users.noreply.github.com>
2024-11-26 02:19:06 +0000

    variables and data types done

--- 01-basics/02-variables.js
+++ 01-basics/02-variables.js
@@ -1,1 +1,6 @@
-console.log("myname");
+ "use strict"; //Uses to use newer version of JS
+console.log(3+3);
+console.log("moonservices")
+let age=12;
+console.log(typeof age );
+console.log(typeof "moonservices");
