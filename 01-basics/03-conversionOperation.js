02f47022730be67b2abd6018493718058df73a73
omar538 <125133978+omar538@users.noreply.github.com>
2024-12-12 16:48:40 +0000

    conversion done

---
+++ 01-basics/03-conversionOperation.js
@@ -0,0 +1,12 @@
+// let score = false;
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let a = Number(score);
+// console.log(typeof a);
+// console.log(a);
+
+let a ="Umar"
+let b = Boolean(a)
+console.log(b)
